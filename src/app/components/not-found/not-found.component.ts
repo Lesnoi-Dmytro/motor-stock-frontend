d6d18420a983086487bf7ea1814cf0f9d6950734
Lesnoi Dmytro <dmytrolesnoi@gmail.com>
2025-02-12 14:04:35 +0200

    feat: implemented not found page

---
+++ src/app/components/not-found/not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [MatCardModule, MatButtonModule, RouterLink],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+  host: {
+    class: 'main_layout',
+  },
+})
+export class NotFoundComponent {}
